6dde97824ba0bf844e5addd1e8e63cb4d7d89664
anivegesana <anirudh.vegesana@gmail.com>
2021-03-21 16:19:58 -0400

    Small modifications

--- distance_estimate.py
+++ distance_estimate.py
@@ -1,12 +1,14 @@
+import fractions
 import math
 
-aspect_ratio: float = float(input('aspect ratio: '))
-fov: float = float(input('field of vision: '))
-h: float = float(input('distance from foot to horizon: '))
-a: float = float(input('height of object: '))
-Hc: float = float(input('camera height: '))
+aspect_ratio: float = float(fractions.Fraction(input('aspect ratio: ')))
+fov: float = math.radians(float(input('field of vision (degrees): ')))
+h: float = float(input('distance from foot to horizon (unit): '))
+a: float = float(input('height of object (unit): '))
+Hc: float = float(input('camera height (meter): '))
+theta: float = math.radians(float(input('theta (degrees): ')))
 
 lens_scaling = math.hypot(1, aspect_ratio) / math.tan(fov / 2)
 scaled_height = lens_scaling * a / h
-distance = 1 / (math.cos(theta) ** 2) * Hc * scaled_height - Hc * tan(theta)
+distance = 1 / (math.cos(theta) ** 2) * Hc * scaled_height - Hc * math.tan(theta)
 print(distance)
