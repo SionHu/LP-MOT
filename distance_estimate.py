6e71ecaa79f939f1a5bef5f24c482ea784be9df5
anivegesana <anirudh.vegesana@gmail.com>
2021-03-21 16:59:27 -0400

    Sample input

--- distance_estimate.py
+++ distance_estimate.py
@@ -1,3 +1,13 @@
+"""
+Sample input
+4/3
+84
+3.7
+7.17
+15.4
+-12.7
+"""
+
 import fractions
 import math
 
@@ -6,9 +16,9 @@ fov: float = math.radians(float(input('field of vision (degrees): ')))
 h: float = float(input('distance from foot to horizon (pixel): '))
 H: float = float(input('height of image (pixel): '))
 Hc: float = float(input('camera height (meter): '))
-theta: float = math.radians(float(input('gimbal pitch (degrees): ')) + 90)
+theta: float = math.radians(-float(input('gimbal pitch (degrees): ')))
 
 lens_scaling = math.hypot(1, aspect_ratio) / math.tan(fov / 2)
 scaled_height = lens_scaling * (H / 2) / h
-distance = 1 / (math.cos(theta) ** 2) * Hc * scaled_height - Hc * math.tan(theta)
+distance = Hc * scaled_height / (math.cos(theta) ** 2) - Hc * math.tan(theta)
 print(f'Distance: {distance}')
