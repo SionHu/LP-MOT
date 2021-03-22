932bbdad0434acb06f8f6c0aa269cc85071de91d
anivegesana <anirudh.vegesana@gmail.com>
2021-03-21 21:48:04 -0400

    Horizon estimation

--- distance_estimate.py
+++ distance_estimate.py
@@ -13,12 +13,13 @@ import math
 
 aspect_ratio: float = float(fractions.Fraction(input('aspect ratio: ')))
 fov: float = math.radians(float(input('field of vision (degrees): ')))
-h: float = float(input('distance from foot to horizon (pixel): '))
+h: float = float(input('lower ordinate from top of the image (pixel): '))
 H: float = float(input('height of image (pixel): '))
 Hc: float = float(input('camera height (meter): '))
 theta: float = math.radians(-float(input('gimbal pitch (degrees): ')))
 
 lens_scaling = math.hypot(1, aspect_ratio) / math.tan(fov / 2)
-scaled_height = lens_scaling * (H / 2) / h
+horizon = (H / 2) * (1 - math.tan(theta) * lens_scaling)
+scaled_height = lens_scaling * (H / 2) / (h - horizon)
 distance = Hc * scaled_height / (math.cos(theta) ** 2) - Hc * math.tan(theta)
 print(f'Distance: {distance}')
